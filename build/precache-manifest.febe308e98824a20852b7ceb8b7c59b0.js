2a5509d12ed2441f2e07169076177293570d4858
Thanasak <Thanasaksrisaeng@gmail.com>
2021-05-12 14:21:14 +0700

    add

---
+++ build/precache-manifest.febe308e98824a20852b7ceb8b7c59b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9be3998d13544a23d18573de45ae18",
+    "url": "/emoji-pedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/emoji-pedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "415803bf7b05a92d4eba",
+    "url": "/emoji-pedia/static/js/main.f02345a7.chunk.js"
+  },
+  {
+    "revision": "cb9718f8a17046bce73b",
+    "url": "/emoji-pedia/static/js/runtime-main.86caf2c5.js"
+  }
+]);
